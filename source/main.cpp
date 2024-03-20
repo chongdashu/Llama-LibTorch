bbde9f139b6e41627d51314ceb50c493049b1c73
Chong-U Lim <me@chongdashu.com>
2024-03-20 14:24:08 +0000

    Update script

--- source/main.cpp
+++ source/main.cpp
@@ -1,41 +1,19 @@
 #pragma once
-#include "llama_causal_lm.h"
+#include "llama_utils.h"
 
 
 void main(int argc, const char* argv[]) {
 
+    if (argc != 3) {
+        std::cout << "Usage: " << argv[0] << " <source_path> <destination_path>" << std::endl;
+        return;
+    }
 
-    // create a config for small model just for testing
-    int vocab_size = 10000;
-    int hidden_size = 64;
-    int num_hidden_layers = 2;
-    int num_attention_heads = 2;
-    int intermediate_size = 512;
-    int max_position_embeddings = 512;
+    std::string sourcePath = argv[1];
+    std::string destPath = argv[2];
 
-    LlamaConfig config = LlamaConfig();
-    config.vocab_size = vocab_size;
-    config.hidden_size = hidden_size;
-    config.num_hidden_layers = num_hidden_layers;
-    config.num_attention_heads = num_attention_heads;
-    config.intermediate_size = intermediate_size;
-    config.max_position_embeddings = max_position_embeddings;
-
-
-    auto model = LlamaCausalLM(config);
-
-    // test generate function
-
-    // set eval mode
-    model->eval();
-
-    // call generate
-    auto input_ids = torch::randint(0, vocab_size, {1, 1});
-
-
-
-    auto output = model->generate(input_ids, 10);
-
-    std::cout << output << std::endl;
+    auto weights = torch::pickle_load(GetTheBytes(sourcePath)).toGenericDict();
+    auto arch = toOutputArchive(weights);
+    arch.save_to(destPath);
 
 }
